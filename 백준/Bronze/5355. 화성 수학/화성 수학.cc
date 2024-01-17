a5fecd1481c0ce9b36e5e6c256140cfa24c85fe3
Heeseo <149475096+hestop@users.noreply.github.com>
2024-01-17 18:42:00 +0900

    [Bronze II] Title: 화성 수학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5355. 화성 수학/화성 수학.cc
+++ 백준/Bronze/5355. 화성 수학/화성 수학.cc
@@ -5,28 +5,24 @@ using namespace std;
 
 int main() {
     int count;
-    cin >> count;
+    cin>>count;
 
-    for (int i = 0; i < count; i++) {
+    for(int i=0;i<count;i++)
+    {
         float num;
-        cin >> num;
+        cin>>num;
         char input;
-
-        cin.ignore();
-
-        while (1) {
+        while (1)
+        {
             input = cin.get();
-            if (input == '\n') break;
-
-            if (input == '@') num *= 3;
-            else if (input == '%') num += 5;
-            else if (input == '#') num -= 7;
+            if(input == '\n') break;
+            if(input == '@') num*=3;
+            else if(input == '%') num+=5;
+            else if(input == '#') num-=7;
         }
-
         cout << fixed;
         cout.precision(2);
         cout << num << endl;
     }
-
     return 0;
 }
